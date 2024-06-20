6d182a7110fb5e6af9821c297c733ddaeea0f34e
louna <r.arswendo.r@gmail.com>
2024-06-20 23:40:53 +0700

    added basic imaging and vector3 type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod vector;
+
 fn main() {
-    println!("Hello, world!");
+    const IMAGE_HEIGHT: i32 = 256;
+    const IMAGE_WIDTH: i32 = 256;
+    println!("P3");
+    println!("{} {}", &IMAGE_HEIGHT, &IMAGE_WIDTH);
+    println!("255");
+
+    for j in 0..IMAGE_HEIGHT {
+        eprintln!("Rendering.... {} Lines Remaining", &IMAGE_HEIGHT - j);
+        for i in 0..IMAGE_WIDTH {
+            let normalized_r: f32 = i as f32 / (IMAGE_WIDTH - 1) as f32;
+            let normalized_g: f32 = j as f32 / (IMAGE_HEIGHT - 1) as f32;
+            let normalized_b: f32 = 0.0;
+
+            let r: i32 = (255.999 * normalized_r) as i32;
+            let g: i32 = (255.999 * normalized_g) as i32;
+            let b: i32 = (255.999 * normalized_b) as i32;
+            print!("\n{} {} {}", r, g, b)
+        }
+    }
+    eprintln!("Rendering finished!");
 }
---
+++ src/vector.rs
@@ -0,0 +1,129 @@
+use std::ops;
+
+pub struct Vector3(f32, f32, f32);
+
+impl Vector3 {
+    fn x(&self) -> f32 {
+        self.0
+    }
+
+    fn y(&self) -> f32 {
+        self.1
+    }
+
+    fn z(&self) -> f32 {
+        self.2
+    }
+
+    fn length(&self) -> f32 {
+        self.squared().sqrt()
+    }
+
+    fn squared(&self) -> f32 {
+        (self.x() * self.x()) + (self.y() * self.y()) + (self.z() * self.z())
+    }
+
+    fn dot(&self, v: &Vector3) -> f32 {
+        (self.x() * v.x()) + (self.y() * v.y()) + (self.z() * v.z())
+    }
+
+    fn cross(&self, v: &Vector3) -> Vector3 {
+        Vector3(
+            self.y() * v.z() - self.z() * v.y(),
+            self.z() * v.x() - self.x() * v.z(),
+            self.x() * v.y() - self.y() * v.x()
+        )
+    }
+
+    fn unit_vector(v: Vector3) -> Vector3 {
+        let mut v_clone = Vector3(v.x(), v.y(), v.z());
+        v_clone / v.length()
+    }
+}
+
+impl ops::Neg for Vector3 {
+    type Output = Vector3;
+
+    fn neg(self) -> Self::Output {
+        Vector3(-self.0, -self.1, -self.2)
+    }
+}
+
+impl ops::Add<Vector3> for Vector3 {
+    type Output = Vector3;
+
+    fn add(self, rhs: Vector3) -> Self::Output {
+        Vector3(self.x() + rhs.x(), self.y() + rhs.y(), self.z() + rhs.z())
+    }
+}
+
+impl ops::AddAssign<Vector3> for Vector3 {
+    fn add_assign(&mut self, rhs: Vector3) {
+        self.0 = self.x() + rhs.x();
+        self.1 = self.y() + rhs.y();
+        self.2 = self.z() + rhs.z();
+    }
+}
+
+impl ops::Sub<Vector3> for Vector3 {
+    type Output = Vector3;
+
+    fn sub(self, rhs: Vector3) -> Self::Output {
+        Vector3(self.x() - rhs.x(), self.y() - rhs.y(), self.z() - rhs.z())
+    }
+}
+
+impl ops::SubAssign<Vector3> for Vector3 {
+    fn sub_assign(&mut self, rhs: Vector3) {
+        self.0 = self.x() - rhs.x();
+        self.1 = self.y() - rhs.y();
+        self.2 = self.z() - rhs.z();
+    }
+}
+
+impl ops::Mul<f32> for Vector3 {
+    type Output = Vector3;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Vector3(self.x() * rhs, self.y() * rhs, self.z() * rhs)
+    }
+}
+
+impl ops::MulAssign<f32> for Vector3 {
+    fn mul_assign(&mut self, rhs: f32) {
+        self.0 = self.x() * rhs;
+        self.1 = self.y() * rhs;
+        self.2 = self.z() * rhs;
+    }
+}
+
+impl ops::Mul<Vector3> for Vector3 {
+    type Output = Vector3;
+
+    fn mul(self, rhs: Vector3) -> Self::Output {
+        Vector3(self.x() * rhs.x(), self.y() * rhs.y(), self.z() * rhs.z())
+    }
+}
+
+impl ops::MulAssign<Vector3> for Vector3 {
+    fn mul_assign(&mut self, rhs: Vector3) {
+        self.0 = self.x() * rhs.x();
+        self.1 = self.y() * rhs.y();
+        self.2 = self.z() * rhs.z();
+    }
+}
+
+impl ops::Div<f32> for Vector3 {
+    type Output = Vector3;
+
+    fn div(self, rhs: f32) -> Self::Output {
+        self * (1.0/rhs)
+    }
+}
+
+impl ops::DivAssign<f32> for Vector3 {
+    fn div_assign(&mut self, rhs: f32) {
+        *self *= 1.0/rhs;
+    }
+}
+
